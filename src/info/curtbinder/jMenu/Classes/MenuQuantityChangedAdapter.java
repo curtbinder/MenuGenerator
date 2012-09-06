43b6458ddac9bbad1a2f0d4b74236b468b8c6185
Curt Binder <curt.binder@gmail.com>
2012-09-05 21:42:31 -0400

    Added copyright notice to source files

--- src/info/curtbinder/jMenu/Classes/MenuQuantityChangedAdapter.java
+++ src/info/curtbinder/jMenu/Classes/MenuQuantityChangedAdapter.java
@@ -1,5 +1,13 @@
 package info.curtbinder.jMenu.Classes;
 
+/*
+ * Copyright (c) 2012 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is made available under the terms of the 
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 import info.curtbinder.jMenu.UI.MainFrame;
 import info.curtbinder.jMenu.UI.MenuApp;
 
--- src/info/curtbinder/jMenu/UI/PredefinedMenu.java
+++ src/info/curtbinder/jMenu/UI/PredefinedMenu.java
@@ -1,5 +1,13 @@
 package info.curtbinder.jMenu.UI;
 
+/*
+ * Copyright (c) 2012 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is made available under the terms of the 
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 import info.curtbinder.jMenu.Classes.AddFunctionAdapter;
 
 import javax.swing.JMenu;
